687776495b0ef3848091ffc79a11c937b202440a
VasylenckoSlava <slavavasylencko@gmail.com>
2018-02-13 00:57:44 +0200

    Create redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
-  render() {
-    return <div>Hello World</div>;  
-  }
+    render() {
+     console.log(this.props.testStore);
+      return (
+        <div>
+          <input type="text"/>
+          <button>Add track</button>
+          <ul>
+            {this.props.testStore.map((track, index) =>
+              <li key={index}>{track}</li>
+        )}
+          </ul>  
+        </div>
+      );    
+    }
 }
 
-export default App;
+export default connect (
+  state => ({
+    testStore: state      
+  }),
+  dispatch =>({})
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const initialstate = [
+  'Smells like spirit',
+   'Enter SanDman'
+];
+function playlist(state = initialstate, action) {
+  if (action.type === 'ADD_TRACK') {
+    return [
+      ...state,
+        action.payload
+      ];
+    }  
+    return state;
+}
+const store = createStore(playlist);
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>,
+document.getElementById('root')
+);
+

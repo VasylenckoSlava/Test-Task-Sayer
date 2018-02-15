0d88da9d1a846f2541c1ea7dd55a9a72e7be0b9e
VasylenckoSlava <slavavasylencko@gmail.com>
2018-02-15 12:33:05 +0200

    Create combine reducers and add findTracks method

--- src/App.js
+++ src/App.js
@@ -2,25 +2,56 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class App extends Component {
+  addTrack() {
+      console.log('addTrack',this.trackInput.value);
+      this.props.onAddTrack(this.trackInput.value);
+      this.trackInput.value = '';
+  }
+
+  findTrack() {
+    console.log('findTrack',this.searchInput.value);
+    this.props.onFindTrack(this.searchInput.value);
+
+  }
+
     render() {
-     console.log(this.props.testStore);
+     console.log(this.props.tracks);
       return (
         <div>
-          <input type="text"/>
-          <button>Add track</button>
+          <div>
+             <input type="text" ref={(input) => { this.trackInput = input}} />
+          <button onClick={this.addTrack.bind(this)}>Add news</button>
+          </div>
+
+           <div>
+              <input type="text" ref={(input) => { this.searchInput = input}} />
+          <button onClick={this.findTrack.bind(this)}>Find news</button>
+            </div>
           <ul>
-            {this.props.testStore.map((track, index) =>
-              <li key={index}>{track}</li>
-        )}
-          </ul>  
+            {this.props.tracks.map((track, index) =>
+              <li key={index}>{track.name}</li>
+            )}
+          </ul>
         </div>
-      );    
+      );
     }
 }
 
 export default connect (
   state => ({
-    testStore: state      
-  }),
-  dispatch =>({})
+  tracks: state.tracks.filter(track => track.name.includes(state.filterTracks))
+}),
+  dispatch =>({
+    onAddTrack: (name) => {
+     const payload = {
+       id: Date.now().toString(),
+        name
+     };
+      dispatch({type: 'ADD_TRACK', payload});
+    },
+    onFindTrack: (name) => {
+      console.log('name', name);
+      dispatch({type:'FIND_TRACKS', payload: name})
+    }
+  })
 )(App);
--- src/index.js
+++ src/index.js
@@ -2,29 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
+import reducer from './reducers';
 //import registerServiceWorker from './registerServiceWorker';
 
-const initialstate = [
-  'Smells like spirit',
-   'Enter SanDman'
-];
-function playlist(state = initialstate, action) {
-  if (action.type === 'ADD_TRACK') {
-    return [
-      ...state,
-        action.payload
-      ];
-    }  
-    return state;
-}
-const store = createStore(playlist);
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-<Provider store={store}>
-<App />
-</Provider>,
+ <Provider store={store}>
+   <App />
+ </Provider>,
 document.getElementById('root')
 );
-
